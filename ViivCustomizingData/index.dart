986417401f5e2012d09d1cf64c270b0c9a11e849
Pyong-Hwa Baek <barkbaek@gmail.com>
2022-01-10 18:58:16 +0900

    [백병화] 01-10 ViivCustomizingData 클래스 변경 - getter, setter 추가

--- ViivCustomizingData/index.dart
+++ ViivCustomizingData/index.dart
@@ -15,4 +15,13 @@ class ViivCustomizingData {
     this._slides = slides,
     this._transitions = transitions,
     this._music = music;
+
+  List<VCSlide> get slides { return this._slides; }
+  set slides(List<VCSlide> value) { this._slides = value; }
+
+  List<VCTransition> get transitions { return this._transitions; }
+  set transitions(List<VCTransition> value) { this._transitions = value; }
+
+  List<VCMusic> get music { return this._music; }
+  set music(List<VCMusic> value) { this._music = value; }
 }
